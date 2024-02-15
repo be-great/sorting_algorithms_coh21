3d7a5cf6f7bde7078466a532657676a22cb5dd21
be-great <begreat5333@gmail.com>
2024-02-16 02:33:53 +0300

    1-O adding

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sort value of node using insertion algorithm
+ * @list: the double linkind list
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+
+
+
+}
--- 1-main.c
+++ 1-main.c
@@ -3,13 +3,13 @@
 #include "sort.h"
 
 /**
- * create_listint - Creates a doubly linked list from an array of integers
- *
- * @array: Array to convert to a doubly linked list
- * @size: Size of the array
- *
- * Return: Pointer to the first element of the created list. NULL on failure
- */
+* create_listint - Creates a doubly linked list from an array of integers
+*
+* @array: Array to convert to a doubly linked list
+* @size: Size of the array
+*
+* Return: Pointer to the first element of the created list. NULL on failure
+*/
 listint_t *create_listint(const int *array, size_t size)
 {
 	listint_t *list;
@@ -34,14 +34,15 @@ listint_t *create_listint(const int *array, size_t size)
 }
 
 /**
- * main - Entry point
- *
- * Return: Always 0
- */
+* main - Entry point
+*
+* Return: Always 0
+*/
 int main(void)
 {
 	listint_t *list;
 	int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+
 	size_t n = sizeof(array) / sizeof(array[0]);
 
 	list = create_listint(array, n);
