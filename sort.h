06da2e25731559f18640050d3493924772758e4f
Isaiah woko <wokoisaiah68@gmail.com>
2024-02-16 22:16:43 +0100

    added 100-shell_sort.c

--- sort.h
+++ sort.h
@@ -26,4 +26,5 @@ void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 #endif /*SORT_H*/
