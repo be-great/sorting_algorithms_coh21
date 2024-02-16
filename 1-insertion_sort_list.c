7ebb7b4da3c539cd9ab45f9117d660a407574a64
be-great <begreat5333@gmail.com>
2024-02-16 05:38:41 +0300

    task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,13 +1,75 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - sort value of node using insertion algorithm
- * @list: the double linkind list
- */
+* swap_nodes - function that swap two node
+* @list: the headder
+* @node0: node 1
+* @node1: node 1
+*/
+void swap_nodes(listint_t **list, listint_t **node0, listint_t **node1)
+{
+	listint_t *prev_one, *prev_two;
+	listint_t *next_one, *next_two;
+	int yes = 0;
+
+	if (*list == *node0)
+		yes = 1;
+	prev_one = *node1;
+	prev_two = (*node0)->prev;
+	next_one = (*node1)->next;
+	next_two = *node0;
+
+	/*swap here*/
+	(*node0)->prev = prev_one;
+	(*node0)->next = next_one;
+	(*node1)->prev = prev_two;
+	(*node1)->next = next_two;
+	/*update the chaining*/
+	if (prev_two != NULL && next_one != NULL)
+	{
+		prev_two->next = *node1;
+		next_one->prev = *node0;
+	}
+	else if (prev_two != NULL)
+	{
+		prev_two->next = *node1;
+	}
+	else
+	{
+	next_one->prev = *node0;
+	}
+	/*update the head if node0 was the head*/
+	if (yes == 1)
+		*list = *node1;
+
+}
+
+/**
+* insertion_sort_list - sort value of node using insertion algorithm
+* @list: the double linkind list
+*/
 
 void insertion_sort_list(listint_t **list)
 {
 
+	listint_t *current, *prev, *temp, *next;
+
+	current = (*list)->next;
+	while (current != NULL)
+	{
+
+		temp = current;
+		next = current->next;
+		prev = current->prev;
+		while (prev != NULL && prev->n > temp->n)
+		{
+			swap_nodes(list, &prev, &temp);
+			prev = temp->prev;
+			print_list(*list);
+		}
+
+		current = next;
+	}
 
 
 }
--- sort.h
+++ sort.h
@@ -23,5 +23,5 @@ void print_array(const int *array, size_t size);
 /*SORTING_ALGORITHMS*/
 void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
-
+void insertion_sort_list(listint_t **list);
 #endif /*SORT_H*/
