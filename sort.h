1f7a29d162d51f175a61b7bd1e23a3b150bfc0e8
Isaiah woko <wokoisaiah68@gmail.com>
2024-02-17 20:35:46 +0100

    OKAY

--- sort.h
+++ sort.h
@@ -29,4 +29,5 @@ void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
+int check_max(int *array, size_t size);
 #endif /*SORT_H*/
