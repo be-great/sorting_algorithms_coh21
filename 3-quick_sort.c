688352270fa49572f2c572643dfbe9fcb2da8e58
be-great <begreat5333@gmail.com>
2024-02-17 06:13:40 +0300

    task 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -28,7 +28,7 @@ int lomuto_partition(int *array, int low, int high, size_t size)
 
 	for (j = low; j <= high - 1; j++)
 	{
-		if (array[j] < pivot)
+		if (array[j] <= pivot)
 		{
 			i++;
 			if (i != j)
@@ -73,7 +73,7 @@ void quick_sort_helper(int *array, int low, int high, size_t size)
 */
 void quick_sort(int *array, size_t size)
 {
-	if (array == NULL || size <= 1)
+	if (array == NULL || size < 2)
 	{
 		return;
 	}
