f4d774749b329e5f303c3f0b1732f065a596750f
be-great <begreat5333@gmail.com>
2024-02-20 07:11:39 +0300

    init task 101

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+ * cocktail_sort_list - function that sorts a doubly linked list of
+ * integers in ascending order
+ * using the Cocktail shaker sort algorithm
+ * @list: the list
+ */
+void cocktail_sort_list(listint_t **list)
+{
+
+}
