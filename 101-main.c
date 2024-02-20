e6157ebc2b74e386b599a25d77eb160e27120689
be-great <begreat5333@gmail.com>
2024-02-20 11:04:52 +0300

    update 101-main.c

--- 101-main.c
+++ 101-main.c
@@ -41,7 +41,7 @@ listint_t *create_listint(const int *array, size_t size)
 int main(void)
 {
 	listint_t *list;
-	int array[] ={19, 1};
+	int array[] = {19, 1};
 	size_t n = sizeof(array) / sizeof(array[0]);
 
 	list = create_listint(array, n);
